a1c6921b8a48a751ed31495aab2455c816b4ff30
kamoladdin <kamoladdin@gmailo.com>
2025-02-01 17:56:56 +0500

    navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,7 +5,7 @@ export default class index extends Component {
   render() {
     return (
       <div>
-        <nav className='container'>
+        <nav className='border-b border-black'>
             <div className='bg-black relative py-[12px]'>
                 <p className='text-[14px] absolute left-[50%] text-white translate-[50%] -translate-[50%]'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
                 <select name="" id="">
@@ -13,8 +13,10 @@ export default class index extends Component {
                     <option value="">Russian</option>
                 </select>
             </div>
+            <div className='container flex items-center justify-between pt-[40px] py-[16px]'>
+
             <NavLink className='text-[24px] font-bold' to={'/'}>Exclusive</NavLink>
-            <ul>
+            <ul className='flex gap-[48px]'>
                 <NavLink to={'/'}>Home</NavLink>
                 <NavLink to={''}>Contact</NavLink>
                 <NavLink to={''}>About</NavLink>
@@ -22,6 +24,7 @@ export default class index extends Component {
             </ul>
             <div>
                 <input type="text" placeholder='What are you looking for?' />
+            </div>
             </div>
         </nav>
       </div>
