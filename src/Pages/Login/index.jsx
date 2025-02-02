7f6485ee204b20643c127c8df19a76bcf05482ff
sobirjon <sobirjonotajonov@gmail.com>
2025-02-02 12:04:56 +0500

    Login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import login from '../../assets/Login.png'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <div>
+        <div className='flex items-center gap-[149px]'>
+          <img src={login} alt="Login"/>
+
+          <form className='flex flex-col'>
+            <h1 className='font-medium text-[36px] mb-[24px]'>Log in to Exclusive</h1>
+            <h3 className='text-[16px] font-400 mb-[48px]'>Enter your details below</h3>
+            <input className='mb-[40px] border' type="text" placeholder='Email or Phone Number'/>
+            <input type="password" placeholder='Password'/>
+          </form>
+        </div>
+      </div>
     )
   }
 }
