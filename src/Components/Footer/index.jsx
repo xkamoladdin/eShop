509eb3b5931d76b51af229af17ed2d59e8570145
kamoladdin <kamoladdin#gmail.com>
2025-02-03 21:57:12 +0500

    footer-start

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <div className='container'>
+            <div>
+                <h2>Exclusive</h2>
+                <p>Subscribe</p>
+                <p>Get 10% off your first order</p>
+                <div>
+                    <input type="text" name="" id="" placeholder='Enter your email' />
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
