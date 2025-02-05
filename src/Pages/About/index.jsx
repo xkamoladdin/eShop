ec26674c4dadbb4e879aafc443bfd51fb7816dc4
Arrow <azizbek@gmail.com>
2025-02-05 21:39:45 +0500

    About

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,70 +1,23 @@
 import React, { Component } from 'react'
-import Logo from '../../assets/react.svg'
+import Side from '../../assets/Side.png'
 
 export default class index extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className='container'>
+        <div className='flex justify-between items-center my-[143px] space-x-[75px]'>
+          <div className='max-w-[525px] w-full'>
+            <h2 className='text-[54px] font-semibold'>Our Story</h2>
+            <p className='my-[40px] text-[16px]'>Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an 
+              active presense in Bangladesh. Supported by wide range of tailored marketing, data and service 
+              solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region.</p>
+              <p className='my-[24px] text-[16px]'>Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
+          </div>
           <div>
-            <h1>Our Story</h1>
-            <p>Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with
-              an active presense in Bangladesh. Supported by wide range of tailored marketing
-              , data and service solutions, Exclusive has 10,500 sallers and
-              300 brands and serves 3 millioons customers across the region. </p>
-            <p>Exclusive has more than 1 Million products to offer
-              , growing at a very fast. Exclusive offers a diverse assotment
-              in categories ranging  from consumer.</p>
-          </div>
-          <img src={Logo} alt="" />
-        </div>
-
-        <br />
-        <br />
-        <br />
-
-        <div>
-          <div className='bg:white hover:red'>
-            <img src="" alt="" />
-            <h3>10.5k </h3>
-            <p>Sallers active our site</p>
-          </div>
-
-
-          <div className='bg:white hover:red'>
-            <img src="" alt="" />
-            <h3>10.5k </h3>
-            <p>Sallers active our site</p>
-          </div>
-
-
-          <div className='bg:white hover:red'>
-            <img src="" alt="" />
-            <h3>10.5k </h3>
-            <p>Sallers active our site</p>
-          </div>
-
-
-          <div className='bg:white hover:red'>
-            <img src="" alt="" />
-            <h3>10.5k </h3>
-            <p>Sallers active our site</p>
-          </div>
-
-
-          <div className='bg:white hover:red'>
-            <img src="" alt="" />
-            <h3>10.5k </h3>
-            <p>Sallers active our site</p>
+            <img src={Side} alt="" />
           </div>
         </div>
-
-
-        
-
       </div>
-
-
     )
   }
 }
