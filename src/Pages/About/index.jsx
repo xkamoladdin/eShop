f280666ec18b2d90937ff9ef8ba7377d9a529004
Arrow <azizbek@gmail.com>
2025-02-05 22:42:09 +0500

    aboutcardsqoldi

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -4,6 +4,9 @@ import Group from '../../assets/Group.png'
 import Moneybag from '../../assets/Moneybag.png'
 import Sale from '../../assets/Sale.png'
 import Shopping from '../../assets/Shopping.png'
+import delivery from '../../assets/delivery.png'
+import secure from '../../assets/secure.png'
+import Customer from '../../assets/Customer.png'
 
 export default class index extends Component {
   render() {
@@ -42,6 +45,28 @@ export default class index extends Component {
             <img className='bg-[#000] p-[12px] my-[24px] mx-[100px] rounded-full' src={Moneybag} alt="" />
             <h4>25k</h4>
             <p>Anual gross sale in our site</p>
+          </div>
+        </div>
+
+        <h1 className='text-center my-[150px] font-bold'>CARD YASANGLAR</h1>
+
+        <div className="container flex justify-between text-center">
+          <div className='flex my-[10px] mx-[70px]'>
+              <div>
+                  <img className='bg-[#000] rounded-full p-[12px] my-[24px] mx-[140px]' src={delivery} alt="" />
+                  <h3 className='font-semibold text-[20px]'>FREE AND FAST DELIVERY</h3>
+                  <p>Free delivery for all orders over $140</p>
+              </div>
+              <div>
+                  <img className='bg-[#000] rounded-full p-[12px] my-[24px] mx-[140px]' src={Customer} alt="" />
+                  <h3 className='font-semibold text-[20px]'>24/7 CUSTOMER SERVICE</h3>
+                  <p>Friendly 24/7 customer support</p>
+              </div>
+              <div>
+                  <img className='bg-[#000] rounded-full p-[12px] my-[24px] mx-[140px]' src={secure} alt="" />
+                  <h3 className='font-semibold text-[20px]'>MONEY BACK GUARANTEE</h3>
+                  <p>We reurn money within 30 days0</p>
+              </div>
           </div>
         </div>
       </div>
