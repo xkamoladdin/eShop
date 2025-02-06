6fecb349238d0e8c3813e2dc67c8b3f781394853
sobirjon <sobirjonotajonov11@gmail.com>
2025-02-06 21:28:50 +0500

    contact end

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
+import { Phone } from 'lucide-react'
+import { Mail } from 'lucide-react';
 
 export default class index extends Component {
   render() {
     return (
-      <div>Contact</div>
+      <div className='container mt-[181px] mb-[140px]'>
+        <div className='flex gap-[30px]'>
+        <div className='contact max-w-[355px] w-full pt-[40px] pl-[35px] pb-[51px] pr-[20px]'>
+          <div>
+      <span className='flex items-center gap-[16px] mb-[24px]'>
+        <span className='bg-[#DB4444] text-white w-[40px] h-[40px] flex justify-center items-center rounded-full' >
+      <Phone/>
+        </span>
+      Call To Us
+      </span>
+      <p className='mb-[16px]'>We are available 24/7, 7 days a week.</p>
+      <p className='pb-[32px] border-b-2'>Phone: +8801611112222</p>
+          </div>
+
+
+          <div className='mt-[32px]'>
+      <span className='flex items-center gap-[16px] mb-[24px]'>
+        <span className='bg-[#DB4444] text-white w-[40px] h-[40px] flex justify-center items-center rounded-full' >
+      <Mail />
+        </span>
+        Write To US
+      </span>
+      <p className='mb-[16px]'>Fill out our form and we will contact you within 24 hours.</p>
+      <p className='mb-[16px]'>Emails: customer@exclusive.com</p>
+      <p>Emails: support@exclusive.com</p>
+          </div>
+        </div>
+
+
+        <form className='contact px-[31px] py-[40px]'>
+            <div className='flex gap-[16px] mb-[32px]'>
+            <input className='bg-[#F5F5F5] px-[16px] py-[13px] rounded' type="text" placeholder='Name'/>
+            <input className='bg-[#F5F5F5] px-[16px] py-[13px] rounded' type="email" placeholder='Your Email'/>
+            <input className='bg-[#F5F5F5] px-[16px] py-[13px] rounded' type="phone" placeholder='Your Phone'/>
+            </div>
+            {/* <input className='bg-[#F5F5F5] px-[16px] pt-[13px] pb-[170px] rounded w-full' type="massage" placeholder='Your Massage'/> */}
+
+            <textarea name="" id="" cols="30" rows="10" className='bg-[#F5F5F5] px-[16px] h-[207px] py-[13px] rounded w-full resize-none' placeholder='Your Massage'></textarea>
+            <div className='flex justify-end mt-[32px]'>
+              <button className='bg-[#DB4444] text-white py-[15px] px-[40px] rounded-[5px]'>Send Massage</button>
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }
--- src/Pages/Singup/index.jsx
+++ src/Pages/Singup/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import login from '../../assets/Login.png'
+import google from '../../assets/google.png'
+import { NavLink } from 'react-router-dom'
 
 export default class Login extends Component {
   render() {
@@ -12,13 +14,19 @@ export default class Login extends Component {
           <form className='flex flex-col'>
             <h1 className='font-medium text-[34px] mb-[24px]'>Create an account</h1>
             <h3 className='text-[16px] font-400 mb-[48px]'>Enter your details below</h3>
+            <input className='mb-[40px] border-b-2' type="text" placeholder='Name'/>
             <input className='mb-[40px] border-b-2' type="text" placeholder='Email or Phone Number'/>
             <input className='border-b-2' type="password" placeholder='Password'/>
-            <div className='flex items-center gap-[47px] mt-[40px]'>
-          <button className='bg-[#DB4444] text-white py-[15px] px-[40px] w-[135px] rounded-[5px]'>Login</button>
-          <button className='bg-none text-[#DB4444]'>Forget Password?</button>
+            <div className='mt-[40px]'>
+          <button className='bg-[#DB4444] text-white py-[15px] px-[40px] w-full rounded-[5px]'> Create Account</button>
+          <button className=' flex  gap-[16px] bg-none text-[#DB4444] mt-[16px]  w-full border-4 py-[16px] px-[56px] rounded-[5px]'> <img src={google} alt="" />Sign up with Google</button>
            </div>
-
+            <div className='flex gap-[16px] justify-center mt-[34px]'>
+            <p>Already have account?</p>
+            <NavLink to={'/login'} className="border-b-2 border-black">
+              Login
+            </NavLink>
+            </div>
           </form>
             </div>
         </div>
